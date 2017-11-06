51bbe9a630cf7a57bb45b962fe6ed5670e772638
xjwStudio <1521975316@qq.com>
2017-11-06 14:13:58 +0800

    更新

--- app/src/main/java/cn/xjw/mvp/base/App.java
+++ app/src/main/java/cn/xjw/mvp/base/App.java
@@ -9,16 +9,17 @@ import android.app.Application;
 
 public class App extends Application {
 
+    private static App app;
+
     @Override
     public void onCreate() {
         super.onCreate();
-
-
+        app = this;
     }
 
-
-
-
+    public static App getApp() {
+        return app;
+    }
 
 
 }
