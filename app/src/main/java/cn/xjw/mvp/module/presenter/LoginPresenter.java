b657c7ac09685aa0585082c7ee377238d09c3338
xjwStudio <1521975316@qq.com>
2017-11-08 15:39:58 +0800

    更新

--- app/src/main/java/cn/xjw/mvp/module/presenter/LoginPresenter.java
+++ app/src/main/java/cn/xjw/mvp/module/presenter/LoginPresenter.java
@@ -66,7 +66,7 @@ public class LoginPresenter extends BasePresenter<LoginContract.View> implements
 
             @Override
             public void end() {
-                view.cancelLoadingDai();
+                view.cancelLoadingDia();
             }
         });
 
